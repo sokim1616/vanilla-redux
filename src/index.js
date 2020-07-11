8c78080a5427f646fa4122284bbe213b2cbe3c99
Macintosh HD <sokim1616@gmail.com>
2020-07-11 20:34:39 +0900

    Modified to a simpler procedure

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ const add = document.getElementById('add');
 const subtract = document.getElementById('subtract');
 const number = document.querySelector('span');
 
+number.innerText = 0; // countModifier에서 default parameter를 넣어줬기 때문에 딱히 필요하진 않음!
+
 //const reducer = () => { }; //reducer is a function that modifies your data && whatever the reducer returns, it's going to be the data that's going to be on the application
 const countModifier = (count = 0, action) => { // 1st parameter: "state" - countModifier (reducer) changes your data & at the same time, whatever it returns, that is the data that's on your application! && 2nd parameter: "action" is how we modify the "state" / it is the second parameter / "action" is the way we communicate with the count modifier because we need a way to tell the "countModifier" that it should return a certain return value
     //... modify count    // ONLY ONE FUNCTION, countModifier (reducer), modifies the data / where "count" is, is where the "state" of the application is supposed to be
-    console.log(count, action);
+    console.log("여길봐라!", count, action);
     if (action.type === 'ADD') {
         return count + 1;
     }
@@ -22,10 +24,23 @@ const countModifier = (count = 0, action) => { // 1st parameter: "state" - count
 //const store = createStore(reducer)
 const countStore = createStore(countModifier); // "countStore" 라는 변수에 "countModifier" 라는 함수를 담는 store를 담아줌!
 
-countStore.dispatch({ type: 'ADD' }) // HOW to send an action to "countModifier"?? When there is a "store", "dispatch", and "action" -> BEHIND THE SCENE: Redux will call "countModifier" like such: countModifier(currentState = 0, { type: 'ADD' })
-// "dispatch" sends messages to "countModifier" && whatever the countModifier returns, that becomes the data
-countStore.dispatch({ type: 'ADD' })
-countStore.dispatch({ type: 'ADD' })
-countStore.dispatch({ type: 'SUBTRACT' })
+const onChange = () => {
+    //console.log(countStore.getState())
+    number.innerText = countStore.getState()
+}
+countStore.subscribe(onChange)
 
-console.log(countStore.getState());
+// countStore.dispatch({ type: 'ADD' }) // HOW to send an action to "countModifier"?? When there is a "store", "dispatch", and "action" -> BEHIND THE SCENE: Redux will call "countModifier" like such: countModifier(currentState = 0, { type: 'ADD' })
+// * "dispatch" sends messages to "countModifier" && whatever the countModifier returns, that becomes the data
+// countStore.dispatch({ type: 'ADD' })
+// countStore.dispatch({ type: 'ADD' })
+// countStore.dispatch({ type: 'SUBTRACT' })
+
+// console.log(countStore.getState());
+
+add.addEventListener("click", () => countStore.dispatch({ type: 'ADD' }));
+subtract.addEventListener("click", () => countStore.dispatch({ type: 'SUBTRACT' }))
+
+
+// "dispatch" function is the way we communicate with the store
+// "subscribe" function allows us to listen to changes in our store
